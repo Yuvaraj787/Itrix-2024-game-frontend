d970b93b00a9fcd468e66f54ce041cf3ac5d170d
Yuvaraj V <yuvarajv787@gmail.com>
2025-03-21 13:10:59 +0530

    mapping to new subdomain

--- src/OwnComponents/variables.tsx
+++ src/OwnComponents/variables.tsx
@@ -1,6 +1,6 @@
 var url1 = "http://localhost:80";
 var url2 = "http://192.168.84.168:80";
 var url3 = "https://game.istaceg.in"
-var url4 = "https://20.55.232.236:3000";
+var url4 = "https://api.istaceg.in";
 
 export default url4;
