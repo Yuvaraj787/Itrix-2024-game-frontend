f4b5146e425a8e3b85b77a8047c9dd441ed5132f
Yuvaraj V <yuvarajv787@gmail.com>
2025-03-21 13:04:19 +0530

    trying 3000

--- src/OwnComponents/variables.tsx
+++ src/OwnComponents/variables.tsx
@@ -1,6 +1,6 @@
 var url1 = "http://localhost:80";
 var url2 = "http://192.168.84.168:80";
 var url3 = "https://game.istaceg.in"
-var url4 = "https://20.55.232.236:443";
+var url4 = "https://20.55.232.236:3000";
 
 export default url4;
