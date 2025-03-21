03494c6d0926ebb0fbb741b11a7fe3e5b58b758c
Yuvaraj V <yuvarajv787@gmail.com>
2025-03-21 13:24:42 +0530

    back to htto

--- src/OwnComponents/variables.tsx
+++ src/OwnComponents/variables.tsx
@@ -1,6 +1,6 @@
 var url1 = "http://localhost:80";
 var url2 = "http://192.168.84.168:80";
 var url3 = "https://game.istaceg.in"
-var url4 = "https://api.istaceg.in";
+var url4 = "http://api.istaceg.in";
 
 export default url4;
