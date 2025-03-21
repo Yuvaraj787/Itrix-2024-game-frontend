c5458c44b9e02fadec67994e61fa154c7c5e8df7
Yuvaraj V <yuvarajv787@gmail.com>
2025-03-21 11:12:27 +0530

    url change

--- src/OwnComponents/variables.tsx
+++ src/OwnComponents/variables.tsx
@@ -1,5 +1,6 @@
 var url1 = "http://localhost:80";
 var url2 = "http://192.168.84.168:80";
 var url3 = "https://game.istaceg.in"
+var url4 = "http://20.55.232.236/"
 
-export default url2;
+export default url4;
